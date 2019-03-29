6241976a363d758189e7addead2b73b92b82092f
Peter Liu <Peter.Liu@afusion.com>
2019-03-29 17:42:43 +0800

    add jekyll-admin dashboard

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ group :jekyll_plugins do
 	gem "jekyll-feed"
 	gem "jemoji"
 	gem "jekyll-include-cache"
+	gem "jekyll-admin"
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
         "url": "http://localhost:4000/lyhistory.github.io/jekyll/update/welcome-to-jekyll/",
+        "teaser":null},{
+        "title": "hello btc",
+        "excerpt":"test admin  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/lyhistory.github.io/hi-btc/",
         "teaser":null}]
