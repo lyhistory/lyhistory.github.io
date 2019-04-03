511a7a607c5e03d7d5589a56676902344d8bce2b
Peter Liu <Peter.Liu@afusion.com>
2019-04-03 13:11:30 +0800

    new post 20190403

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,15 @@ var store = [{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "http://localhost:4000/lyhistory.github.io/jekyll/update/welcome-to-jekyll/",
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
         "teaser":null},{
         "title": "hello btc",
         "excerpt":"test admin  ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/lyhistory.github.io/hi-btc/",
+        "url": "http://localhost:4000/hi-btc/",
+        "teaser":null},{
+        "title": "Setup dev env - virtualbox",
+        "excerpt":"It’s important for developers to setup local env to simulate production env, not only that we have a overview(god’s eye view) of the whole project but also we can have a ‘mini lab’ to try out and get the result quickly. And for blockchain developers it’s even crutical, someone may...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/env-virtualbox/",
         "teaser":null}]
