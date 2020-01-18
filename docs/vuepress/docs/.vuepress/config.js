dc1fb2e02f8a65a6bba1e598eb29bebd69ffcf1e
lyhistory <lyhistory@gmail.com>
2020-01-18 21:07:25 +0800

    init docs with vuepress
    delete docsify as it conflicts with jekyll which my blog is using
    now,because jekyll ignore every file/folder with undersocre proceeding,
    but unluckly docsify use _navbar.md _sidebar.md;
    now I try out vuepress, see whether it works, looks like it can
    generate static pages, guess it will work

---
+++ docs/vuepress/docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base: '/docs/',
+  themeConfig: {
+    nav: [
+      { text: '机器指令', link: '/' },
+      { text: '渗透测试', link: '/coder2hacker/' },
+      { text: '区块链', link: '/blockchain/' }
+    ]
+  }
+}
---
+++ docs/vuepress/docs/blockchain/README.md
@@ -0,0 +1,10 @@
+---
+home: false
+heroImage: /hero.png
+heroText: Hero Title
+tagline: Hero subtitle
+actionText: Get Started →
+actionLink: /blockchain/
+footer: MIT Licensed | Copyright © 2018-LIU YUE
+---
+
